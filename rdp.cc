26bee01abe4e25a0b2615848f1a4a76a2f1fd374
Joseph Passalacqua <JVPass@gmail.com>
2020-06-23 00:46:37 -0400

    updates cpp file
    added addition of setLookAhead(). Compiles but prints no output

--- rdp.cc
+++ rdp.cc
@@ -17,22 +17,28 @@ bool checkEqualsOperand();
 bool E();
 bool O();
 bool P();
-bool leftPerentecyCheck();
-bool rightPerentecyCheck();
+bool leftParenthesesCheck();
+bool rightParenthesesCheck();
 bool U();
 bool I();
 bool C();
 bool L();
 bool D();
+string printParseString();
 bool lookAheadIsInUOperators();
 bool checkForSpecialCaseExponent();
 bool lookAheadIsInOoperators();
 bool lookAheadIsInDigits();
-bool incrementLookAheadPosition();
+void incrementLookAheadPosition();
 int getLookAheadPosition();
+//int getLookAheadPosition(int);
+bool getLookAheadPositions(int);
 char getLookAheadCharacter();
 bool isLookAheadPositionValid();
+bool isLookAheadPositionValid(int);
 bool lookAheadIsInAlphabet();
+bool setLookAheadPosition(int);
+int i;
 string s;
 char alphabet[26];
 char digits[10];
@@ -61,104 +67,194 @@ rdp::rdp ():alphabet
 ,s("")
 {
    "";}
-
+int i = 0;
+int setLookAheadPosition = 0;
 int lookAheadPosition = 0;
-
-
-bool
-rdp:: parseString(string s)
+   
+rdp::~rdp()
 {
-    int count = 0;
-	cout << s + " is the string we are parsing";
-	//setLookAheadPosition(0);
+}
+
+
+
+bool rdp:: parseString(string s){
+	    
+	     setLookAheadPosition(0);
+	    	 return A() || A();
+	}
+	//special override dont use unless in return regex
+
+bool rdp:: A() {
+		if(I())
+			if(checkEqualsOperand()) 
+				if(E()) {
+					//incrementLookAheadPosition();
+					if(!isLookAheadPositionValid())
+						return true;
+					else 
+						return A();
+				}
+		
+		
+		
+		return false;
+		
+		
+		
+	}
 	
-	 if(A()){
-	   cout << s+ " is in the language";
-	   return true;
-	  }
-	   else
-	   { cout << s + " is NOT in the language";
+bool rdp:: E() {
+		int temp = getLookAheadPosition();
+		if(P()) 
+			if(O())
+				if(P())
+					return true;
+				else 
+					setLookAheadPosition(temp);
+			else 
+				setLookAheadPosition(temp);
+		if(P())
+			return true;
+		else
+			setLookAheadPosition(temp);
+		return false;
+		
+		//return(P() && O() && P()|| P());
+	}
+	
+bool rdp:: O() {
+		return lookAheadIsInOoperators();
+	}
+	
+bool rdp:: P() {
+		int temp = getLookAheadPosition();
+		if(I()) 
+			return true;
+		if(L())
+			return true;
+		if(U()) {
+			if(I())
+				return true;
+		
+		else
+			setLookAheadPosition(temp);
+		}
+		if(U()) {
+			if(L())
+				return true;
+		}
+		else
+			setLookAheadPosition(temp);
+		if(leftParenthesesCheck()) {
+			if(E()) {
+				if(rightParenthesesCheck())
+					return true;
+			}
+			else {
+				setLookAheadPosition(temp);
+			}
+		}
+		else {
+			setLookAheadPosition(temp);
+		}
+		return false;
+					
+		//return (I() || L() || U() && I()|| U() && L() || leftParenthesesCheck() && E() && rightParenthesesCheck()); 
+	}
+	
+bool rdp:: U() {
+		return lookAheadIsInUOperators();
+	}
+	
+bool rdp:: I() {
+	
+		int temp = getLookAheadPosition();
+		if(C())
+			return true;
+		if(C()) {
+			if(I())
+				return true;
+		}
+		else {
+			setLookAheadPosition(temp);
+		}
+		
+		return false;
+		//return(C() ||  C() && I());
+	}
+	
+bool rdp:: C() {
+		return lookAheadIsInAlphabet();
+	}
+	
+bool rdp:: L() {
+		int temp = getLookAheadPosition();
+		if(D())
+			return true;
+		if(D())
+			if(L())
+				return true;
+		setLookAheadPosition(temp);
+		return false;
+		//return (D() || D() && L() );
+	}
+	
+bool rdp:: D() {
+		return (lookAheadIsInDigits());
+	}
+	
+bool rdp:: checkEqualsOperand(){
+		
+        	if(isLookAheadPositionValid() && getLookAheadCharacter() == '='){ 
+        	incrementLookAheadPosition();  
+        	return true; 
+        	}
+        	else {
+        	    return false;
+        	}
+	}
+	//use push and pop eventually this is buggy
+bool rdp:: leftParenthesesCheck(){
+	    if(isLookAheadPositionValid() && getLookAheadCharacter() == '('){
+	        incrementLookAheadPosition(); 
+	        return true;
+	    } 
+	    else {
+	        return false;
+	    }
+	}
+	//use push and pop eventually this is buggy
+bool rdp:: rightParenthesesCheck(){
+	    
+	    if ( isLookAheadPositionValid() && getLookAheadCharacter() == ')'){ 
+	        incrementLookAheadPosition();
+	        return true;
+	    }
+	    else{
+	      return false;  
+	    } 
+	}
+	
+bool rdp:: lookAheadIsInAlphabet(){
+	    
+	    if(isLookAheadPositionValid()){
+	    	for (char c: alphabet) {
+	    		if(c == getLookAheadCharacter()) {
+	    		incrementLookAheadPosition();
+	    		return true;
+	    		}
+	    	}
+	  		 //iterate through Uoperator list return true if found
+
+	    }
+	    
 	    return false;
-	   }
-}
-
-bool
-rdp:: A ()
-{
-   return (I() && checkEqualsOperand() && E()); 
-}
-
-bool
-rdp:: E()
-{
-    return(P() && O() && P() || P());
-}
-
-bool
-rdp:: O()
-{
-    return lookAheadIsInOoperators();
-}
-
-bool
-rdp:: P()
-{
-   return I() || L() || U() && I() || U() && L() || (leftPerentecyCheck() && E() && rightPerentecyCheck()); 
-}
-
-bool
-rdp:: U()
-{
-    return lookAheadIsInUOperators();
-}
-
-
-bool
-rdp:: I()
-{
-    return(C() ||  C() && I());
-}
-
-bool
-rdp:: C()
-{
-    return lookAheadIsInAlphabet();
-}
-
-bool
-rdp:: L()
-{
-    return (D() || D() && L());
-}
-
-bool
-rdp:: D()
-{
-    return (lookAheadIsInDigits());
-}
-
-bool
-rdp:: checkEqualsOperand()
-{
-    return (getLookAheadCharacter() == '=' && incrementLookAheadPosition());
-}
-
-bool
-rdp:: leftPerentecyCheck()
-{
-    return (getLookAheadCharacter() == '(' && incrementLookAheadPosition());
-}
-
-bool
-rdp:: rightPerentecyCheck()
-{
-    return (getLookAheadCharacter() == ')'&& incrementLookAheadPosition());
-}
-
-bool
-rdp:: lookAheadIsInUOperators ()
-{
-    if(isLookAheadPositionValid()){
+	    
+	}
+	
+bool rdp:: lookAheadIsInUOperators(){
+	    
+	    if(isLookAheadPositionValid()){
 	    	for (char c: Uoperators) {
 	    		if(c == getLookAheadCharacter()) {
 	    		incrementLookAheadPosition();
@@ -166,109 +262,111 @@ rdp:: lookAheadIsInUOperators ()
 	    		}
 	    	}
             return false;
-	    }
+	    }	
 	    return false;
 	    
-}
-
-bool
-rdp:: lookAheadIsInOoperators ()
-{
-     if(isLookAheadPositionValid()){
-	    	for (char c: Ooperators) {
-	    		if(c == getLookAheadCharacter() || checkForSpecialCaseExponent()) {
-	    		
-	    		incrementLookAheadPosition();
-	    		return true;
-	    		}
-	    	}
-	    	return false;
-	    }
-	    return false;
-}
-
-bool
-rdp:: checkForSpecialCaseExponent()
-{
-   if (s.at(getLookAheadPosition()) == '*' && s.at(getLookAheadPosition()+1) == '*') {
+	}
+	
+bool rdp:: checkForSpecialCaseExponent() {
+		if(s.at(getLookAheadPosition()) == '*' && s.at(getLookAheadPosition()+1) == '*') {
 			incrementLookAheadPosition();
 			return true;
 		}
-		else {
-		}
-		return false; 
-}
-
-bool
-rdp:: lookAheadIsInAlphabet()
-{
-    if(isLookAheadPositionValid()){
-	    	for (char c: alphabet) {
-	    		if(c == getLookAheadCharacter()) {
+		return false;	
+	}
+	
+bool rdp:: lookAheadIsInOoperators(){
+	    
+	    if(isLookAheadPositionValid()){
+	    	for (char c: Ooperators) {
+	    		if(c == getLookAheadCharacter()|| checkForSpecialCaseExponent()) { //check for**
 	    		incrementLookAheadPosition();
 	    		return true;
 	    		}
 	    	}
-	  		 
-
+	        
 	    }
-	    
 	    return false;
-}
-
-bool
-rdp:: lookAheadIsInDigits ()
-{
-  if(isLookAheadPositionValid()){
+	}
+	
+bool rdp:: lookAheadIsInDigits(){
+	    
+	    if(isLookAheadPositionValid()){
 	    	for (char c: digits) {
 	    		if( c == getLookAheadCharacter()) {
 	    		incrementLookAheadPosition();
 	    		return true;
 	    		}
 	    	}
-	    	return false;
-
 	    }
-	    return false;  
-}
-
-bool
-rdp:: incrementLookAheadPosition()
-{
-    if(getLookAheadPosition() +1 < s.length()) {
-		++lookAheadPosition;
-		}
-		else{
-		}
-			return false;
-}
-
-int
-rdp:: getLookAheadPosition ()
-{
-    if(isLookAheadPositionValid()){
-		 return lookAheadPosition; 
-		    }
-		  return -1; 
-}
-
-char
-rdp:: getLookAheadCharacter ()
-{
-  return s.at(getLookAheadPosition());  
-}
-
-bool
-rdp:: isLookAheadPositionValid ()
-{
-    if (lookAheadPosition < s.length()){
+	    return false;
+	}
+	
+bool rdp:: isLookAheadPositionValid(int i) {
+		 if (i<s.length()) {
+			 lookAheadPosition =i;
 	        return true;
 	    }
 	    else {
 	       return false;
 	    }
-}
+		
+	}
+	
+bool rdp:: setLookAheadPosition(int i){
+		if(getLookAheadPositions(i)) {
+	    lookAheadPosition =i;
+	    return false;
+		}
+		return true;
+	}
+	
+bool rdp:: getLookAheadPositions(int i) {
+		if(isLookAheadPositionValid(i)) {
+			return true;
+		}
+		return false;
+	}
+	
+void rdp:: incrementLookAheadPosition() {
+		if(isLookAheadPositionValid()) {
+			++lookAheadPosition;
+			}
+		
+	}
 
+int rdp:: getLookAheadPosition() {
+		
+		if(isLookAheadPositionValid()){
+		       return lookAheadPosition; 
+		    }
+		       return -1; 
+	        
+		    }
+	
+char rdp:: getLookAheadCharacter(){
+	       return s.at(getLookAheadPosition()); 
+	    
+	    }
+	
+bool rdp:: isLookAheadPositionValid(){
+	    if (lookAheadPosition >= 0 && lookAheadPosition < s.length()){
+	        return true;
+	    }
+	    else {
+	       return false;
+	    }
+	}
+
+	
+string rdp:: printParseString(){
+		if(isLookAheadPositionValid()) {
+	    return s.substr(getLookAheadPosition(),s.length());
+		}
+		else {
+			return "N/A";
+		}
+	}
 
 int
 main()
@@ -276,5 +374,4 @@ main()
     string str = "a=1";
   rdp *algorithm = new rdp();
   algorithm->parseString(str);
-  return 0;
 }
